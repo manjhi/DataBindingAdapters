88572d34767cbedc90e919f7f962b9bd23085103
Manjinder Singh <30902993+manjhi@users.noreply.github.com>
2021-01-19 16:42:55 +0530

    Create DataBindingAdapter.kt

---
+++ DataBindingAdapter.kt
@@ -0,0 +1,12 @@
+
+abstract class DataBindingAdapter<T>(diffCallback: DiffUtil.ItemCallback<T>) :
+    ListAdapter<T, DataBindingViewHolder<T>>(diffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataBindingViewHolder<T> {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = DataBindingUtil.inflate<ViewDataBinding>(layoutInflater, viewType, parent, false)
+        return DataBindingViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: DataBindingViewHolder<T>, position: Int) = holder.bind(getItem(position))
+}
