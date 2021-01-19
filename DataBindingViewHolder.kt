affd6667cbbf20bfbff05aa6132469d6364f4e01
Manjinder Singh <30902993+manjhi@users.noreply.github.com>
2021-01-19 16:43:18 +0530

    Create DataBindingViewHolder.kt

---
+++ DataBindingViewHolder.kt
@@ -0,0 +1,8 @@
+class DataBindingViewHolder<T>(private val binding: ViewDataBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(item: T) {
+        binding.setVariable(BR.item, item)
+        binding.executePendingBindings()
+    }
+}
